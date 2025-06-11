0a532621c70da1438cb05de85dece46a599428c7
wantedbear007 <pratapsinghbhanu444@gmail.com>
2025-06-11 23:08:19 +0530

    fix: social handle

--- src/constant/envvariables.ts
+++ src/constant/envvariables.ts
@@ -1,7 +1,7 @@
 export const ENV = {
   NEXT_PUBLIC_API_BACKEND_URL: 'https://api-clans.kilt.io',
   NEXT_PUBLIC_API_BASE_URL: 'https://clans.kilt.io',
-  NEXT_PUBLIC_X_HANDLER: '@JoinTheClan',
+  NEXT_PUBLIC_X_HANDLER: '@JoinTheClans',
 };
 // export const ENV = {
 //   NEXT_PUBLIC_API_BACKEND_URL: 'https://clans.10on10studios.com',
